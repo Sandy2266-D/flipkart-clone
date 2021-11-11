46d17109c276983d6d768c8416ada5f143873fc6
sandy2266-D <santhoshdurai2266@gmail.com>
2021-11-11 08:07:19 +0530

    v1.0.0

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { v4 as uuid } from 'uuid';
+import DefaultData from './default.js';
+import Routes from './routes/userRoutes.js';
+import Connection from './database/db.js';
+
+dotenv.config();
+const app = express();
+const PORT=process.env.PORT || 5000;
+
+
+Connection();
+
+
+app.listen(PORT,()=>console.log("server listening"))
+
+DefaultData();
+
+app.use(cors())
+app.use(express.json())
+app.use('/', Routes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('cilent/build'))
+}
+
+export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+paytmParams['MID'] = process.env.PAYTM_MID,
+paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
+paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
+paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
+paytmParams['ORDER_ID'] = uuid(),
+paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
+paytmParams['TXN_AMOUNT'] = '500',
+paytmParams['CALLBACK_URL'] = 'http://localhost:5000/callback',
+paytmParams['EMAIL'] = 'santhosdura@gmail.com',
+paytmParams['MOBILE_NO'] = '1234567898'
+
+
